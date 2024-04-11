8a3d560bb6e4a18a25418bfe153c20d3e9b2bc6f
Kage7047 <kage7047@gmail.com>
2024-04-11 17:33:51 +0200

    Fixed ID generation

--- VetClinicAPI/Models/Visit.cs
+++ VetClinicAPI/Models/Visit.cs
@@ -6,6 +6,5 @@ public class Visit
     public DateTime DateOfVisit { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
-    public int AnimalId { get; set; }
 }
 
